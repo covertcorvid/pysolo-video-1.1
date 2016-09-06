4ad430749b343ef4418810c9751069bda8f1c2e6
covertcorvid <claughrey12@gmail.com>
2016-09-06 16:31:03 -0400

    panel 2 update monitor #

--- pvg_common.py
+++ pvg_common.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 #       pvg_common.py
-#       
+#
 #       Copyright 2011 Giorgio Gilestro <giorgio@gilest.ro>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -38,107 +38,107 @@ class myConfig():
         temporary   whether we are reading and storing values temporarily
         defaultOptions  a dict containing the defaultOptions
         """
-        
+
         filename = filename or DEFAULT_CONFIG
         pDir = os.getcwd()
         if not os.access(pDir, os.W_OK): pDir = os.environ['HOME']
 
         self.filename = os.path.join (pDir, filename)
         self.filename_temp = '%s~' % self.filename
-        
+
         self.config = None
-        
-        if defaultOptions != None: 
+
+        if defaultOptions != None:
             self.defaultOptions = defaultOptions
         else:
             self.defaultOptions = { "option_1" : [0, "Description"],
                                     }
-        
+
         self.Read(temporary)
 
     def New(self, filename):
         """
         """
         self.filename = filename
-        self.Read()  
+        self.Read()
 
     def Read(self, temporary=False):
         """
         read the configuration file. Initiate one if does not exist
-        
+
         temporary       True                Read the temporary file instead
                         False  (Default)     Read the actual file
         """
 
         if temporary: filename = self.filename_temp
-        else: filename = self.filename        
-        
+        else: filename = self.filename
+
         if os.path.exists(filename):
             self.config = ConfigParser.RawConfigParser()
-            self.config.read(filename)   
-            
+            self.config.read(filename)
+
         else:
             self.Save(temporary, newfile=True)
 
-                               
+
     def Save(self, temporary=False, newfile=False, filename=None):
         """
         """
-        
+
         if temporary and not filename: filename = self.filename_temp
         elif not temporary and not filename: filename = self.filename
-       
+
         if newfile:
             self.config = ConfigParser.RawConfigParser()
             self.config.add_section('Options')
-            
+
             for key in self.defaultOptions:
                 self.config.set('Options', key, self.defaultOptions[key][0])
 
         with open(filename, 'wb') as configfile:
             self.config.write(configfile)
-    
+
         if not temporary: self.Save(temporary=True)
 
 
     def SetValue(self, section, key, value):
         """
         """
-        
+
         if not self.config.has_section(section):
             self.config.add_section(section)
-        
+
         self.config.set(section, key, value)
-        
+
     def GetValue(self, section, key):
         """
         get value from config file
-        Does some sanity checking to return tuple, integer and strings 
+        Does some sanity checking to return tuple, integer and strings
         as required.
         """
         r = self.config.get(section, key)
-        
+
         if type(r) == type(0) or type(r) == type(1.0): #native int and float
             return r
         elif type(r) == type(True): #native boolean
             return r
         elif type(r) == type(''):
             r = r.split(',')
-        
+
         if len(r) == 2: #tuple
             r = tuple([int(i) for i in r]) # tuple
-        
+
         elif len(r) < 2: #string or integer
             try:
                 r = int(r[0]) #int as text
             except:
                 r = r[0] #string
-        
+
         if r == 'False' or r == 'True':
             r = (r == 'True') #bool
-        
+
         return r
-                
+
 
     def GetOption(self, key):
         """
@@ -154,11 +154,11 @@ class acquireObject():
         self.verbose = False
         self.track = track
         outputFile = os.path.join(dataFolder, 'Monitor%02d.txt' % monitor)
-        
+
         self.mon = pv.Monitor()
         self.mon.setSource(source, resolution)
         self.mon.setTracking(True, track_type, mask_file, outputFile)
-        
+
         if self.verbose: print ( "Setting monitor %s with source %s and mask %s. Output to %s " % (monitor, source, os.path.split(mask_file)[1], os.path.split(outputFile)[1] ) )
 
     def run(self, kbdint=False):
@@ -166,7 +166,7 @@ class acquireObject():
         """
         while self.keepGoing:
             self.mon.GetImage()
-                
+
     def start(self):
         """
         """
@@ -193,29 +193,29 @@ class acquireThread(threading.Thread):
         self.verbose = False
         self.track = track
         outputFile = os.path.join(dataFolder, 'Monitor%02d.txt' % monitor)
-        
+
         self.mon = pv.Monitor()
         self.mon.setSource(source, resolution)
         self.mon.setTracking(True, track_type, mask_file, outputFile)
-        
+
         if self.verbose: print ( "Setting monitor %s with source %s and mask %s. Output to %s " % (monitor, source, os.path.split(mask_file)[1], os.path.split(outputFile)[1] ) )
 
     def run(self, kbdint=False):
         """
         """
-        
+
         if kbdint:
-        
+
             while self.keepGoing:
                 try:
                     self.mon.GetImage()
                 except KeyboardInterrupt:
                     self.halt()
-                    
+
         else:
             while self.keepGoing:
                 self.mon.GetImage()
-                
+
     def doTrack(self):
         """
         """
@@ -234,12 +234,12 @@ class pvg_config(myConfig):
     """
     def __init__(self, filename=None, temporary=False):
 
-        
+
         defaultOptions = { "Monitors" : [9, "Select the number of monitors connected to this machine"],
                             "Webcams"  : [1, "Select the number of webcams connected to this machine"],
-                            "ThumbnailSize" : ['320, 240', "Specify the size for the thumbnail previews"], 
-                            "FullSize" : ['640, 480', "Specify the size for the actual acquisition from the webcams.\nMake sure your webcam supports this definition"], 
-                            "FPS_preview" : [5, "Refresh frequency (FPS) of the thumbnails during preview.\nSelect a low rate for slow computers"],  
+                            "ThumbnailSize" : ['320, 240', "Specify the size for the thumbnail previews"],
+                            "FullSize" : ['640, 480', "Specify the size for the actual acquisition from the webcams.\nMake sure your webcam supports this definition"],
+                            "FPS_preview" : [5, "Refresh frequency (FPS) of the thumbnails during preview.\nSelect a low rate for slow computers"],
                             "FPS_recording" : [5, "Actual refresh rate (FPS) during acquisition and processing"],
                             "Data_Folder" : ['', "Folder where the final data are saved"]
                            }
@@ -254,7 +254,7 @@ class pvg_config(myConfig):
         mn = 'Monitor%s' % monitor
         for v, vn in zip( args, self.monitorProperties ):
             self.SetValue(mn, vn, v)
-    
+
     def GetMonitor(self, monitor):
         """
         """
@@ -273,15 +273,15 @@ class pvg_config(myConfig):
 
     def getMonitorsData(self):
         """
-        return a list containing the monitors that we need to track 
+        return a list containing the monitors that we need to track
         based on info found in configfile
         """
         monitors = {}
-        
+
         ms = self.GetOption('Monitors')
         resolution = self.GetOption('FullSize')
         dataFolder = self.GetOption('Data_Folder')
-        
+
         for mon in range(1,ms+1):
             if self.HasMonitor(mon):
                 _,source,track,mask_file,track_type,isSDMonitor = self.GetMonitor(mon)
@@ -293,20 +293,20 @@ class pvg_config(myConfig):
                 monitors[mon]['dataFolder'] = dataFolder
                 monitors[mon]['track'] = track
                 monitors[mon]['isSDMonitor'] = isSDMonitor
-            
+
         return monitors
 
 
-        
+
 class previewPanel(wx.Panel):
     """
-    A panel showing the video images. 
+    A panel showing the video images.
     Used for thumbnails
     """
     def __init__(self, parent, size, keymode=True):
 
         wx.Panel.__init__(self, parent, wx.ID_ANY, style=wx.WANTS_CHARS)
-        
+
         self.parent = parent
 
         self.size = size
@@ -316,7 +316,7 @@ class previewPanel(wx.Panel):
 
         self.SetBackgroundColour('#A9A9A9')
 
-        self.sourceType = 0 
+        self.sourceType = 0
         self.source = ''
         self.mon = None
         self.track = False
@@ -338,7 +338,7 @@ class previewPanel(wx.Panel):
         self.selROI = -1
         self.polyPoints = []
         self.keymode = keymode
-        
+
         self.ACTIONS = {
                         "a": [self.AutoMask, "Automatically create the mask"],
                         "c": [self.ClearLast, "Clear last selected area of interest"],
@@ -348,15 +348,15 @@ class previewPanel(wx.Panel):
                         "s": [self.SaveMask, "Save mask to file"],
                         "q": [self.Stop, "Close connection to camera"]
                         }
-        
+
         self.Bind( wx.EVT_LEFT_DOWN, self.onLeftDown )
         self.Bind( wx.EVT_LEFT_UP, self.onLeftUp )
         self.Bind( wx.EVT_LEFT_DCLICK, self.AddPoint )
         self.Bind( wx.EVT_MOTION, self.onMotion )
         self.Bind( wx.EVT_RIGHT_DOWN, self.ClearLast )
         self.Bind( wx.EVT_MIDDLE_DOWN, self.SaveCurrentSelection )
-        
-        if keymode: 
+
+        if keymode:
             self.Bind( wx.EVT_CHAR, self.onKeyPressed )
             self.SetFocus()
 
@@ -370,11 +370,11 @@ class previewPanel(wx.Panel):
         """
         Cancel current drawing
         """
-       
+
         if self.allowEditing:
             self.selection = None
             self.polyPoints = []
-            
+
             if self.selROI >= 0:
                 self.mon.delROI(self.selROI)
                 self.selROI = -1
@@ -387,29 +387,29 @@ class previewPanel(wx.Panel):
             self.mon.addROI(self.selection, 1)
             self.selection = None
             self.polyPoints = []
-        
+
     def AddPoint(self, event=None):
         """
         Add point
         """
-        
+
         if self.allowEditing:
             if len(self.polyPoints) == 4:
                 self.polyPoints = []
-            
+
             #This is to avoid selecting a neigh. area when drawing point
             self.selection = None
-            self.selROI = -1 
-            
+            self.selROI = -1
+
             x = event.GetX()
             y = event.GetY()
             self.polyPoints.append( (x,y) )
 
-    
+
     def onLeftDown(self, event=None):
         """
         """
-        
+
         if self.allowEditing and self.mon:
             x = event.GetX()
             y = event.GetY()
@@ -420,33 +420,33 @@ class previewPanel(wx.Panel):
             else:
                 self.selection = self.mon.getROI(r)
                 self.selROI = r
-        
+
     def onLeftUp(self, event=None):
         """
         """
         if self.allowEditing:
             self.dragging = None
             self.track_window = self.selection
-            
+
             if len(self.polyPoints) == 4:
                 self.selection = self.polyPoints
                 self.polyPoints = []
-            
+
     def onMotion(self, event=None):
         """
         """
         if self.allowEditing:
             x = event.GetX()
             y = event.GetY()
-            
+
             self.dragging = event.Dragging()
-            
+
             if self.dragging:
                 xmin = min(x, self.startpoints[0])
                 ymin = min(y, self.startpoints[1])
                 xmax = max(x, self.startpoints[0])
                 ymax = max(y, self.startpoints[1])
-                
+
                 x1, y1, x2, y2  = (xmin, ymin, xmax, ymax)
                 self.selection = (x1,y1), (x2,y1), (x2,y2), (x1, y2)
 
@@ -461,22 +461,22 @@ class previewPanel(wx.Panel):
         Regulates key pressing responses:
         """
         key = chr(event.GetKeyCode())
-        
+
         if key == "g" and self.mon.writer: self.mon.grabMovie = not self.mon.grabMovie
 
         if self.ACTIONS.has_key(key):
             self.ACTIONS[key][0]()
-    
+
     def Calibrate(self, event=None):
         """
         """
         if len(self.polyPoints) > 2:
             print "You need only two points for calibration. I am going to use the first two"
-            
+
         pt1, pt2 = self.polyPoints[0], self.polyPoints[1]
         r = self.mon.calibrate(pt1, pt2)
-        self.polyPoints = []  
-        
+        self.polyPoints = []
+
         print "%spixels = 1cm" % r
 
     def AutoMask(self, event=None):
@@ -490,13 +490,13 @@ class previewPanel(wx.Panel):
         """
         """
         self.mon.saveROIS()
-        
+
     def setMonitor(self, camera, resolution=None):
         """
         """
-        
+
         if not resolution: resolution = self.size
-        
+
         self.camera = camera
         self.resolution = resolution
         self.mon = pv.Monitor()
@@ -507,7 +507,7 @@ class previewPanel(wx.Panel):
         self.Bind(wx.EVT_PAINT, self.onPaint)
         self.playTimer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.onNextFrame)
-        
+
 
     def paintImg(self, img):
         """
@@ -515,13 +515,13 @@ class previewPanel(wx.Panel):
         if img:
             depth, channels = img.depth, img.nChannels
             datatype = cv.CV_MAKETYPE(depth, channels)
-            
+
             frame = cv.CreateMat(self.size[1], self.size[0], datatype)
             cv.Resize(img, frame)
 
             cv.CvtColor(frame, frame, cv.CV_BGR2RGB)
             #cv.CvtColor(frame, frame, cv.CV_GRAY2RGB)
-            
+
             self.bmp.CopyFromBuffer(frame.tostring())
             self.Refresh()
 
@@ -530,7 +530,7 @@ class previewPanel(wx.Panel):
         """
         if self.bmp:
             dc = wx.BufferedPaintDC(self)
-            self.PrepareDC(dc)
+            #self.PrepareDC(dc)
             dc.DrawBitmap(self.bmp, 0, 0, True)
         evt.Skip()
 
@@ -540,7 +540,7 @@ class previewPanel(wx.Panel):
 
         self.paintImg( self.mon.GetImage(drawROIs = self.drawROI, selection=self.selection, crosses=self.polyPoints, timestamp=self.timestamp) )
         if evt: evt.Skip()
-      
+
     def Play(self, status=True, showROIs=True):
         """
         """
@@ -551,12 +551,12 @@ class previewPanel(wx.Panel):
         if self.mon:
             self.drawROI = showROIs
             self.isPlaying = status
-            
+
             if status:
                 self.playTimer.Start(self.interval)
             else:
                 self.playTimer.Stop()
-            
+
     def Stop(self):
         """
         """
--- pvg_panel_two.py
+++ pvg_panel_two.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 #       untitled.py
-#       
+#
 #       Copyright 2011 Giorgio Gilestro <giorgio@gilest.ro>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -31,7 +31,7 @@ class panelLiveView(wx.Panel):
     def __init__(self, parent):
         """
         """
-        
+
         wx.Panel.__init__(self, parent, wx.ID_ANY)
 
         self.monitor_number = options.GetOption("Monitors")
@@ -39,29 +39,29 @@ class panelLiveView(wx.Panel):
         self.monitor_name = ''
 
         self.fsPanel = previewPanel(self, size=self.fs_size)
-        
+
         sizer_1 = wx.BoxSizer(wx.VERTICAL)
         sizer_2 = wx.BoxSizer(wx.HORIZONTAL)
         sizer_3 = wx.BoxSizer(wx.HORIZONTAL)
         sizer_4 = wx.BoxSizer(wx.VERTICAL)
-        
+
         #Static box1: monitor input
         sb_1 = wx.StaticBox(self, -1, "Select Monitor")#, size=(250,-1))
-        sbSizer_1 = wx.StaticBoxSizer (sb_1, wx.VERTICAL)
+        self.sbSizer_1 = wx.StaticBoxSizer (sb_1, wx.VERTICAL)
         self.MonitorList = ['Monitor %s' % (int(m) + 1) for m in range(self.monitor_number)]
         self.thumbnailNumber = wx.ComboBox(self, -1, size=(-1,-1) , choices=self.MonitorList, style=wx.CB_DROPDOWN | wx.CB_READONLY | wx.CB_SORT)
         self.Bind(wx.EVT_COMBOBOX, self.onChangeMonitor, self.thumbnailNumber)
 
         self.sourceTXTBOX =  wx.TextCtrl (self, -1, "No monitor selected", style=wx.TE_READONLY)
 
-        sbSizer_1.Add ( self.thumbnailNumber, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
-        sbSizer_1.Add ( self.sourceTXTBOX, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
+        self.sbSizer_1.Add ( self.thumbnailNumber, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
+        self.sbSizer_1.Add ( self.sourceTXTBOX, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
 
         #Static box2: mask parameters
         sb_2 = wx.StaticBox(self, -1, "Mask Editing")#, size=(250,-1))
         sbSizer_2 = wx.StaticBoxSizer (sb_2, wx.VERTICAL)
         fgSizer_1 = wx.FlexGridSizer( 0, 2, 0, 0 )
-        
+
         self.btnClear = wx.Button( self, wx.ID_ANY, label="Clear All")
         self.Bind(wx.EVT_BUTTON, self.fsPanel.ClearAll, self.btnClear)
 
@@ -75,7 +75,7 @@ class panelLiveView(wx.Panel):
         fgSizer_1.Add (self.btnClear)
         fgSizer_1.Add (self.btnClearLast)
         fgSizer_1.Add (self.btnAutoFill)
-        
+
         sbSizer_2.Add (fgSizer_1)
 
 
@@ -92,7 +92,7 @@ class panelLiveView(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.onSaveMask, self.btnSave)
         self.btnSaveApply = wx.Button( self, wx.ID_ANY, label="Save and Apply")
         self.Bind(wx.EVT_BUTTON, self.onSaveApply, self.btnSaveApply)
-        
+
         btnSizer_1.Add(self.btnLoad)
         btnSizer_1.Add(self.btnSave)
         btnSizer_1.Add(self.btnSaveApply)
@@ -101,7 +101,7 @@ class panelLiveView(wx.Panel):
         sbSizer_3.Add (btnSizer_1, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
 
         ##
-        
+
         #Static box4: help
         sb_4 = wx.StaticBox(self, -1, "Help")
         sbSizer_4 = wx.StaticBoxSizer (sb_4, wx.VERTICAL)
@@ -113,27 +113,27 @@ class panelLiveView(wx.Panel):
                   ('Right mouse button - click', 'Remove selected currently selected ROI'),
                   ('Auto Fill', 'Will fill 32 ROIS (16x2) to fit under the last two\nselected points. To use select first upper left corner,\n then the lower right corner, then use "Auto Fill".')
                   ]
-                  
+
         for title, text in instr:
             t = wx.StaticText(self, -1, title); t.SetFont(titleFont)
             sbSizer_4.Add( t, 0, wx.ALL, 2 )
             sbSizer_4.Add(wx.StaticText(self, -1, text) , 0 , wx.ALL, 2 )
             sbSizer_4.Add ( (wx.StaticLine(self)), 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 5 )
-        
-        sizer_4.Add(sbSizer_1, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
+
+        sizer_4.Add(self.sbSizer_1, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
         sizer_4.Add(sbSizer_2, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
         sizer_4.Add(sbSizer_3, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
         sizer_4.Add(sbSizer_4, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP|wx.EXPAND, 5 )
 
-        
+
         sizer_3.Add(self.fsPanel, 0, wx.LEFT|wx.TOP, 20 )
         sizer_3.Add(sizer_4, 0, wx.ALIGN_RIGHT|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
 
         sizer_1.Add(sizer_3, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
         sizer_1.Add(sizer_2, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
 
-        
-        self.SetSizer(sizer_1) 
+
+        self.SetSizer(sizer_1)
 
     def StopPlaying(self):
         """
@@ -146,12 +146,12 @@ class panelLiveView(wx.Panel):
         FIX THIS
         this is a mess
         """
-        
+
         if self.fsPanel.isPlaying: self.fsPanel.Stop()
-        
+
         self.monitor_name = event.GetString()
         self.monitor_number = self.MonitorList.index( self.monitor_name ) + 1
-        
+
         n_cams = options.GetOption("Webcams")
         WebcamsList = [ 'Webcam %s' % (int(w) +1) for w in range( n_cams ) ]
 
@@ -163,7 +163,7 @@ class panelLiveView(wx.Panel):
             if mask_file:
                 self.fsPanel.mon.loadROIS(mask_file)
                 self.currentMaskTXT.SetValue(os.path.split(mask_file)[1] or '')
-            
+
             if sourceType == 0:
                 self.sourceTXTBOX.SetValue( WebcamsList[source] )
             else:
@@ -178,12 +178,12 @@ class panelLiveView(wx.Panel):
         """
         Save ROIs to File
         """
-        
+
         filename = '%s.msk' % self.monitor_name.replace(' ','_')
         wildcard = "pySolo mask file (*.msk)|*.msk"
-        
+
         dlg = wx.FileDialog(
-            self, message="Save file as ...", defaultDir=os.getcwd(), 
+            self, message="Save file as ...", defaultDir=os.getcwd(),
             defaultFile=filename, wildcard=wildcard, style=wx.SAVE
             )
 
@@ -193,7 +193,7 @@ class panelLiveView(wx.Panel):
             path = dlg.GetPath()
             self.fsPanel.mon.saveROIS(path)
             self.currentMaskTXT.SetValue(os.path.split(path)[1])
-        
+
         dlg.Destroy()
         return path
 
@@ -210,9 +210,9 @@ class panelLiveView(wx.Panel):
         """
         Load Mask from file
         """
-        
+
         wildcard = "pySolo mask file (*.msk)|*.msk"
-        
+
         dlg = wx.FileDialog(
             self, message="Choose a file",
             defaultDir=os.getcwd(),
@@ -225,7 +225,15 @@ class panelLiveView(wx.Panel):
             path = dlg.GetPath()
             self.fsPanel.mon.loadROIS(path)
             self.currentMaskTXT.SetValue(os.path.split(path)[1])
-        
-        dlg.Destroy()
-       
 
+        dlg.Destroy()
+
+    def onRefresh(self):
+        if self.monitor_number != options.GetOption("Monitors"):
+            self.monitor_number = options.GetOption("Monitors")
+            self.sbSizer_1.Hide(0)
+            self.sbSizer_1.Remove(0)
+            self.MonitorList = ['Monitor %s' % (int(m) + 1) for m in range(self.monitor_number)]
+            self.thumbnailNumber = wx.ComboBox(self, -1, size=(-1,-1) , choices=self.MonitorList, style=wx.CB_DROPDOWN | wx.CB_READONLY | wx.CB_SORT)
+            self.sbSizer_1.Insert (1, self.thumbnailNumber, 0, wx.ALIGN_CENTRE|wx.LEFT|wx.RIGHT|wx.TOP, 5 )
+            self.sbSizer_1.Layout()
